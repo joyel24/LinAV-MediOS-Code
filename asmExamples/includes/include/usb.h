892b2b3effd8864023c6769df183753d1d84b8d4
oxygen77 <>
2004-02-12 13:42:55 +0000

    extend usb support (from avos module)

--- asmExamples/includes/include/usb.h
+++ asmExamples/includes/include/usb.h
@@ -1,7 +1,6 @@
-/* usb.h
-	By Christophe THOMAS (aka oxygen)
+/* libavos.h
+   By William Bland (aka awksedgrep)
    Copyright 2004, the Avos project.
-   fev 2004
 
    This file is free software; we give unlimited permission to copy
    and/or distribute it, with or without modifications, as long as this
@@ -12,17 +11,12 @@
    even the implied warranty of MERCHANTABILITY or FITNESS FOR A
    PARTICULAR PURPOSE.
 */
-
-
-#ifndef _USB_H_
-#define _USB_H_
-
-// assembly functions
-#ifndef _u32_
-#define _u32_
+#ifndef u32_defined
+#define u32_defined yes
 typedef unsigned long u32;
 #endif
 
-extern u32 usbIsConnectedT();
+extern u32 usbIsConnected();
+extern void usbEnable();
+extern void usbDisable();
 
-#endif
