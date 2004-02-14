f219eacbe7ef17081e15694f38c74120c2a35083
doggermoore <>
2004-02-14 19:28:31 +0000

    Added string.s + .h for doing string things. So far putting hex is done.

--- avos/src/examples/uartEcho.c
+++ avos/src/examples/uartEcho.c
@@ -1,15 +1,19 @@
 #include <uart.h>
-    
+#include <string.h>    
+
 //
 //
 //
 int main() {
     int c;
-    
+    char p[] = "val = [xx] [x]\n";
+
     while(1) {
         c = uartGetch();
         if (c!=-1) {
-            uartOutch(c);    
+            stringPutHex(p + 7, c, 2);
+            p[12] = (char) c;
+            uartOuts(p);
         }
     }
 }
