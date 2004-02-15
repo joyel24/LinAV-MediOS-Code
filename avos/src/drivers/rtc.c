2eee4e9ccb528b582ff7060924f67b4ad117bd85
doggermoore <>
2004-02-15 15:38:02 +0000

    Got rtc drive in, and organised for more c drivers.... Need to modify Makefile though.

---
+++ avos/src/drivers/rtc.c
@@ -0,0 +1,32 @@
+// avOS - http://avos.sourceforge.net
+// Copyright (c) 2003 by Jimmy Moore
+//
+// All files in this archive are subject to the GNU General Public License.
+// See the file COPYING in the source tree root for full license agreement.
+// This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+// KIND, either express of implied.
+//
+// Date:     04/01/2004
+// Author:   By DoggerMoore
+
+#include "i2c.h"
+#include "rtc.h"
+
+#define RTC_ADDR 0xd0
+
+static struct tm tmStatic;
+
+struct tm rtcGetTime() {
+    int c = i2cRead(RTC_ADDR, &tmStatic, 9);
+    
+    // TODO - Handle i2c errors...
+    
+    return tmStatic
+}
+
+void rtcSetTime(struct tm newTime) {
+    int c = i2cWrite(RTC_ADDR, &newTime, 9);
+    
+    // TODO - Handle i2c errors...
+
+}
