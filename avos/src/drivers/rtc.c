d8fab81d44203ed0800671aedbc709f403780776
doggermoore <>
2004-02-15 17:32:14 +0000

    Fixed makefile for c libraries

--- avos/src/drivers/rtc.c
+++ avos/src/drivers/rtc.c
@@ -15,14 +15,13 @@
 #define RTC_ADDR 0xd0
 
 static struct tm tmStatic = {1,0,0,0,0,0,0,0,0};
-static unsigned char tmBuffer[100];
 
 struct tm rtcGetTime() {
-    //int c = i2cRead(RTC_ADDR, 0, tmBuffer, 9);  //&tmStatic, 9);
+    int c = i2cRead(RTC_ADDR, 0, &tmStatic, 9);
     
     // TODO - Handle i2c errors...
     
-    //return tmStatic;
+    return tmStatic;
 }
 
 void rtcSetTime(struct tm newTime) {
