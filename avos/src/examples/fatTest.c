61099488bdcef331aca06edff4dea0cef263ddea
doggermoore <>
2004-02-17 05:58:41 +0000

    fatTest looking pretty good now.

--- avos/src/examples/fatTest.c
+++ avos/src/examples/fatTest.c
@@ -6,6 +6,8 @@
 #include <osdDSC25.h>
 #include <buttons.h>
 #include <fonts.h>
+#include <usb.h>
+#include <rtc.h>
 
 void showBuffer(char *source);
 
@@ -13,7 +15,10 @@ static int pal32[2] = {0x8080c0e0, 0xffffffff};
 static int pal16[2] = {0x0000, 0xffff};
 
 struct graphicsBuffer screenBitmap;
+struct graphicsBuffer screenBitmap2;
 struct graphicsBuffer sprite5_7 = {0, 1, 5, 7, 1, 0, -1, 0, 0, 0, 0, (int**) &pal16, (int**) &pal32};
+struct graphicsBuffer sprite4_6 = {0, 1, 4, 6, 1, 0, -1, 0, 0, 0, 0, (int**) &pal16, (int**) &pal32};
+struct graphicsBuffer spriteShadow = {0, 2, 12, 18, 1, 0, -1, 0, 0, 0, 0, (int**) &pal16, (int**) &pal32};
 
 struct dirEntry dirBuffer[1000];
 struct dirEntry dirBuffer2[1000];
@@ -23,6 +28,9 @@ char hex82[] = "xxxxxxxx";
 
 char dirLine[] = "xxxxxxxx.xxx\n";
 
+struct tm* ourTime;
+    char timeSt[] = "xx:xx:xx.xx";
+
 int main() {
     int c, i, totalEntries;
     int cursorpos=0;
@@ -30,9 +38,14 @@ int main() {
     unsigned int cluster=0;
     int loopDelay = 0xc000;
     int source = 0;
+    int mode = 0;
     int part;
+    int cursorMoved=1;
     
     osdInit();    
+    
+    rtcInit();
+    
     osdSetComponentConfig(OSD_VIDEO1, 0);
     osdSetComponentConfig(OSD_VIDEO2, 0);
     osdSetComponentConfig(OSD_BITMAP1, 0);
@@ -49,12 +62,31 @@ int main() {
 
     graphicsBoxf(&screenBitmap, 0, 0, 96, 132, 0x0202);    
     graphicsBoxf(&screenBitmap, 1, 1, 94, 130, 0x1414);    
-    
-    osdSetComponentSize(OSD_BITMAP1, 96*2, 132);
-    osdSetComponentPosition(OSD_BITMAP1, 0x14 + (2*16), 0x12 + 32);
-    osdSetComponentOffset(OSD_BITMAP1, 0x03e00000);
-    osdSetComponentSourceWidth(OSD_BITMAP1, 6);
+
+    screenBitmap2.offset = 0x03b00000;
+    screenBitmap2.bytesPerLine = 320*2;
+    screenBitmap2.width = 320;
+    screenBitmap2.height =240;
+    screenBitmap2.bitsPerPixelShift = 4;
+    screenBitmap2.bitsPerPixel = 16;
+
+    graphicsBoxf(&screenBitmap2, 0, 0, 320, 240, 0x0000);
+    pal16[0] = 0x0000;
+    pal16[1] = 0xffff;    
+    graphicsString(&screenBitmap2, 2, 231, &sprite4_6, std4x6_, 5, 0,
+                        "[Menu1] HDD/Memcard   [OFF] UsbMode   [ON] Click");
+
+    osdSetComponentSize(OSD_BITMAP1, 320*2, 240);
+    osdSetComponentPosition(OSD_BITMAP1, 0x14, 0x12);
+    osdSetComponentOffset(OSD_BITMAP1, 0x03b00000);
+    osdSetComponentSourceWidth(OSD_BITMAP1, 0x14);
     osdSetComponentConfig(OSD_BITMAP1, OSD_COMPONENT_ENABLE
+                                     | OSD_BITMAP_8BIT);
+    osdSetComponentSize(OSD_BITMAP2, 96*2, 132);
+    osdSetComponentPosition(OSD_BITMAP2, 0x14 + (2*16), 0x12 + 32);
+    osdSetComponentOffset(OSD_BITMAP2, 0x03e00000);
+    osdSetComponentSourceWidth(OSD_BITMAP2, 6);
+    osdSetComponentConfig(OSD_BITMAP2, OSD_COMPONENT_ENABLE
                                      | OSD_BITMAP_MERGEBACK
                                      | OSD_BITMAP_A6
                                      | OSD_BITMAP_8BIT);
@@ -83,58 +115,113 @@ int main() {
 
     while(1) {
 
-        graphicsBoxf(&screenBitmap, 1, 1, 94, 130, 0x1414);    
+        if (mode==0) {
+            graphicsBoxf(&screenBitmap, 1, 1, 94, 130, 0x1414);    
 
-        for (c=0;c<1000;c++) {
-            dirBuffer[c].name[0] = 0;    
-        }
-        
-        c = fatReadFile(cluster, (char*) dirBuffer);
-        stringPutHex(hex82, c, 8);
-        uartOuts("[fatTest.c] fatReadFile returned = ");
-        uartOuts(hex82);
-        uartOuts("\n");
-
-        totalEntries = fatDirFilter(dirBuffer, dirBuffer2, 1000);
-        
-        //showBuffer((char*) dirBuffer2);
-        
-        cursorpos=0;
-        dirpos=0;
-
-        while(1) {
-            
-            for (c=0;c<16;c++) {
-                if ((dirpos+c)>=totalEntries) break;
-
-                for (i=0;i<8;i++) dirLine[i] = dirBuffer2[dirpos+c].name[i];
-                for (i=0;i<3;i++) dirLine[i+9] = dirBuffer2[dirpos+c].ext[i];
-//                uartOuts(dirLine);
-
-                pal16[1] = 0xffff;
-                if (dirBuffer2[dirpos+c].attr & FAT_ATTR_DIR)
-                    pal16[1] = 0x4e4e;
-                if (c==cursorpos) {
-                    pal16[0] = 0x0000;
-                } else {
-                    pal16[0] = 0x1414;            
-                }
-
-                graphicsString(&screenBitmap, 3, 3 + c*8, &sprite5_7, std5x7_, 5, 0,
-                    dirLine);
+            for (c=0;c<1000;c++) {
+                dirBuffer[c].name[0] = 0;    
             }
 
-            do {
-                c =buttonsGetStatus();
-                if (!(c & BUTTONS_AV300_ANY)) loopDelay = 0xc000;
-            } while(!(c & BUTTONS_AV300_ANY));
+            usbDisable();
+        
+            c = fatReadFile(cluster, (char*) dirBuffer);
+            stringPutHex(hex82, c, 8);
+            uartOuts("[fatTest.c] fatReadFile returned = ");
+            uartOuts(hex82);
+            uartOuts("\n");
+
+            totalEntries = fatDirFilter(dirBuffer, dirBuffer2, 1000);
+        
+            //showBuffer((char*) dirBuffer2);
+        
+            cursorpos=0;
+            dirpos=0;
+            osdSetComponentConfig(OSD_BITMAP2, OSD_COMPONENT_ENABLE
+                                     | OSD_BITMAP_MERGEBACK
+                                     | OSD_BITMAP_A6
+                                     | OSD_BITMAP_8BIT);
+        } else {
+            osdSetComponentConfig(OSD_BITMAP2, 0);
+            usbEnable();
+        }
+            
+        while(1) {
+
+            c = usbIsConnected();
+            pal16[0] = 0x0000;            
+            pal16[1] = 0x4e4e;
+            if (c) {
+                graphicsString(&screenBitmap2, 2, 2, &sprite4_6, std4x6_, 5, 0,
+                    "[USB Lead in]");
+                
+            } else {
+                graphicsString(&screenBitmap2, 2, 2, &sprite4_6, std4x6_, 5, 0,
+                    "             ");
+            }
+
+            if (mode!=0) {
+                graphicsString(&screenBitmap2, 2 + 14*5, 2, &sprite4_6, std4x6_, 5, 0,
+                    "[USB mode]");
+                
+            } else {
+                graphicsString(&screenBitmap2, 2 + 14*5, 2, &sprite4_6, std4x6_, 5, 0,
+                    "          ");
+            }
+
+            if (source==0) {
+                graphicsString(&screenBitmap2, 2 + 25*5, 2, &sprite4_6, std4x6_, 5, 0,
+                    "[HDD]    ");
+                
+            } else {
+                graphicsString(&screenBitmap2, 2 + 25*5, 2, &sprite4_6, std4x6_, 5, 0,
+                    "[MemCard]");
+            }
+            
+            ourTime = rtcGetTime();
+            stringPutHex(timeSt+9, ourTime->tm_ms, 2);
+            stringPutHex(timeSt+6, ourTime->tm_sec, 2);
+            stringPutHex(timeSt+3, ourTime->tm_min, 2);
+            stringPutHex(timeSt, ourTime->tm_hour, 2);
+            pal16[1] = 0xffff;
+            graphicsString(&screenBitmap2, 2 + 35*5, 2, &sprite4_6, std4x6_, 5, 0,
+                        timeSt);
+
+            // Update file display window if needed,
+            
+            if (mode==0 && cursorMoved==1) {
+                for (c=0;c<16;c++) {
+                    if ((dirpos+c)>=totalEntries) break;
     
+                    for (i=0;i<8;i++) dirLine[i] = dirBuffer2[dirpos+c].name[i];
+                    for (i=0;i<3;i++) dirLine[i+9] = dirBuffer2[dirpos+c].ext[i];
+    //                uartOuts(dirLine);
+    
+                    pal16[1] = 0xffff;
+                    if (dirBuffer2[dirpos+c].attr & FAT_ATTR_DIR)
+                        pal16[1] = 0x4e4e;
+                    if (c==cursorpos) {
+                        pal16[0] = 0x0000;
+                    } else {
+                        pal16[0] = 0x1414;            
+                    }
+    
+                    graphicsString(&screenBitmap, 3, 3 + c*8, &sprite5_7, std5x7_, 5, 0,
+                        dirLine);
+                }
+                cursorMoved=0;
+            }
+                
+                
+            c =buttonsGetStatus();
+            if (!(c & BUTTONS_AV300_ANY)) loopDelay = 0xc000;
+        
             if (c & BUTTONS_AV300_DOWN) {
                 if ((cursorpos<15) && (cursorpos<(totalEntries-1))) {
                     cursorpos++;
                 } else {
                     if ((dirpos+15)<(totalEntries-1)) dirpos++;
                 }
+                cursorMoved=1;
                 if (loopDelay>=0x1000) loopDelay-=0x1000;
             } else if (c & BUTTONS_AV300_UP) {
                 if (cursorpos>0) {
@@ -143,11 +230,17 @@ int main() {
                     if (dirpos>0) dirpos--;
                 }
                 if (loopDelay>=0x1000) loopDelay-=0x1000;
+                cursorMoved=1;
             } else if (c & BUTTONS_AV300_ON) {
                 if (dirBuffer2[dirpos+cursorpos].attr & FAT_ATTR_DIR) {
                     cluster = dirBuffer2[dirpos+cursorpos].fatCluHI << 16
                         | dirBuffer2[dirpos+cursorpos].fatCluLO;
                     if (cluster==0) cluster=getRootClu();
+                    cursorMoved=1;
+                    do {
+                        c =buttonsGetStatus();
+                    } while(c & BUTTONS_AV300_ANY);                    
+                    break;
                 } else {
                     // TODO
                     //  Handle plugins
@@ -159,6 +252,12 @@ int main() {
                 do {
                     c =buttonsGetStatus();
                 } while(c & BUTTONS_AV300_ANY);
+            } else if (c & BUTTONS_AV300_OFF) {
+                mode ^= 1;
+                do {
+                    c =buttonsGetStatus();
+                } while(c & BUTTONS_AV300_ANY);
+                cursorMoved=1;
                 break;
             } else if (c & BUTTONS_AV300_MENU1) {
                 source ^= 1;    
@@ -189,10 +288,11 @@ int main() {
                 do {
                     c =buttonsGetStatus();
                 } while(c & BUTTONS_AV300_ANY);
+                cursorMoved=1;
                 break;
             }
             for (i=0;i<loopDelay;i++) {}          // Little delay...
-    
+
         }
     }
     
