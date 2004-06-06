28bd7db9374aa8ad71099b41a27d6fb286e8dc61
doggermoore <>
2004-06-06 17:32:30 +0000

    Just a couple of changes...

--- avos2/examples/exploder/exploder.c
+++ avos2/examples/exploder/exploder.c
@@ -3,21 +3,25 @@
 #include <osdDSC25.h>
 #include <fonts.h>
 #include <stdio.h>
+#include <gio.h>
+#include <interrupts.h>
 
-    struct graphicsBuffer screenBitmap;
-    static int pal16[2] = {0x0000, 0xffff};
+struct graphicsBuffer screenBitmap;
+static int pal16[2] = {0x0000, 0xffff};
+struct graphicsBuffer sprite6_9 = {0, 1, 6, 9, 1, 0, -1, 0, 0, 0, 0, (int**) &pal16, 0};
 
-    struct graphicsBuffer sprite4_6 = {0, 1, 4, 6, 1, 0, -1, 0, 0, 0, 0, (int**) &pal16, 0};
-
-void printBuff();
+void exploderShow();
+void exploderButtons();
+void exploderInit();
+void updateInt(int i);
+void intsub();
 
 static char* saddr = (char*) 0x30000;
 static int vaddr = 0;
-
 static int changeaddr = 1;
 
 int main() {
-    int b;
+    int b, mode=0;
 
     osdInitA();
 
@@ -35,6 +39,34 @@ int main() {
     screenBitmap.bitsPerPixelShift = 4;
     screenBitmap.bitsPerPixel = 16;
 
+    // Assume DVR for now...
+    cpldSetModeA(4);
+    cpldSetReg2A(0x0b);
+    cpldSetReg1A(0x06);
+
+    interruptsInitA((void*)intsub);    
+    interruptsSetMaskA(0xffffff7f);
+    interruptsSetIRQEnabledA();
+    gioSetAllIRQsA(0xff);               // Set all gio (0-7) to be IRQ!
+    
+    exploderInit();
+
+    for (b=0;b<32;b++) {
+        updateInt(b);
+    }
+    
+    while(1) {
+        exploderShow();
+        b = buttonsGetStatusA();
+        if (b & BUTTONS_AV300_OFF) return 0;
+
+        exploderButtons(b);        
+    }
+
+}
+
+
+void exploderInit() {
     graphicsBoxfA(&screenBitmap, 0, 0, 320, 240, 0x0000);
 
     osdSetComponentSizeA(OSD_BITMAP1, 320*2, 240);
@@ -44,28 +76,36 @@ int main() {
     osdSetComponentConfigA(OSD_BITMAP1, OSD_COMPONENT_ENABLE
                                      | OSD_BITMAP_8BIT);
 
-    
-    while(1) {
-        printBuff();
-        b = buttonsGetStatusA();
-        if (b & BUTTONS_AV300_OFF) return 0;
-        if (b & BUTTONS_AV300_LEFT) {
-            vaddr--;
-            vaddr = (vaddr & 7);
-        } else if (b & BUTTONS_AV300_RIGHT) {
-            vaddr++;
-            vaddr = (vaddr & 7);
-        } else if (b & BUTTONS_AV300_UP) {
-            if (vaddr==7) saddr -= (1<<(4*(7-vaddr))); 
-            saddr -= (1<<(4*(7-vaddr)));
-            changeaddr = 1;
-        } else if (b & BUTTONS_AV300_DOWN) {
-            if (vaddr==7) saddr += (1<<(4*(7-vaddr))); 
-            saddr += (1<<(4*(7-vaddr)));
-            changeaddr = 1;
-        }
-    }
 
+    graphicsStringA(&screenBitmap, 6, 10, &sprite6_9, std6x9_, 6, 0, "        ");
+    graphicsStringA(&screenBitmap, 6+(6*vaddr), 10, &sprite6_9, std6x9_, 6, 0, "*");
+}
+    
+void exploderButtons(int b) {
+    int delay;
+    if (b & BUTTONS_AV300_LEFT) {
+        vaddr--;
+        vaddr = (vaddr & 7);
+        pal16[1] = 0xffff;
+        graphicsStringA(&screenBitmap, 6, 10, &sprite6_9, std6x9_, 6, 0, "        ");
+        graphicsStringA(&screenBitmap, 6+(6*vaddr), 10, &sprite6_9, std6x9_, 6, 0, "*");
+        for (delay=0;delay<0x8000;delay++) {}
+    } else if (b & BUTTONS_AV300_RIGHT) {
+        vaddr++;
+        vaddr = (vaddr & 7);
+        pal16[1] = 0xffff;
+        graphicsStringA(&screenBitmap, 6, 10, &sprite6_9, std6x9_, 6, 0, "        ");
+        graphicsStringA(&screenBitmap, 6+(6*vaddr), 10, &sprite6_9, std6x9_, 6, 0, "*");
+        for (delay=0;delay<0x8000;delay++) {}
+    } else if (b & BUTTONS_AV300_UP) {
+        if (vaddr==7) saddr -= (1<<(4*(7-vaddr))); 
+        saddr -= (1<<(4*(7-vaddr)));
+        changeaddr = 1;
+    } else if (b & BUTTONS_AV300_DOWN) {
+        if (vaddr==7) saddr += (1<<(4*(7-vaddr))); 
+        saddr += (1<<(4*(7-vaddr)));
+        changeaddr = 1;
+    }    
 }
 
 char* pbuff = "xxxx";
@@ -73,19 +113,17 @@ char* paddr = "xxxxxxxx";
 char* pchars = "xxxxxxxxxxxxxxxx";
 
 static short lastbuff[8*32];
+static unsigned int lastGio = 0;
+static char gioBits[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
-void printBuff() {
+void exploderShow() {
     int y, x, a;
 
-    pal16[1] = 0x0101;
-    graphicsStringA(&screenBitmap, 4, 0, &sprite4_6, std4x6_, 4, 0, "        ");
-    graphicsStringA(&screenBitmap, 4+(4*vaddr), 0, &sprite4_6, std4x6_, 4, 0, "*");
-
-    for (y=0;y<32;y++) {
+    for (y=0;y<16;y++) {
         if (changeaddr==1) {
             sprintf(paddr, "%08X", (int) (saddr + (y<<4)));
-            pal16[1] = 0x0202;
-            graphicsStringA(&screenBitmap, 4, 7*(y+1), &sprite4_6, std4x6_, 4, 0, paddr);
+            pal16[1] = 0xffff;
+            graphicsStringA(&screenBitmap, 6, 10 + 9*(y+1), &sprite6_9, std6x9_, 6, 0, paddr);
         }
 
         pal16[1] = 0xffff;
@@ -93,19 +131,54 @@ void printBuff() {
             a = ((unsigned short*)saddr)[(y<<3) + x];
             if (changeaddr || (a!=lastbuff[x + (y<<3)])) {
                 sprintf(pbuff, "%04X", a);
-                graphicsStringA(&screenBitmap, 4*10+(5*4*x), 7*(y+1), &sprite4_6, std4x6_, 4, 0, pbuff);            
+                graphicsStringA(&screenBitmap, 6*10+(6*5*x), 9*(y+2), &sprite6_9, std6x9_, 6, 0, pbuff);            
             }
             lastbuff[(y<<3) + x] = a;
         }
-            
-        for (x=0;x<16;x++) {            
-            a = saddr[(y<<4) + x];
-            if (a<0x20 || a>0x7f) a='.'; 
-            pchars[x] = a;
-        }
-        pal16[1] = 0x0303;
-        graphicsStringA(&screenBitmap, 4*50, 7*(y+1), &sprite4_6, std4x6_, 4, 0, pchars);
     }
     changeaddr = 0;
+    
+    a = gioGetAllBitsetsA();
+    if (a!=lastGio) {
+        lastGio = a;
+        for (a=0;a<32;a++) {
+            if ((lastGio >> a) & 1)
+                gioBits[a]='1';
+            else
+                gioBits[a]='0';
+        }
+        graphicsStringA(&screenBitmap, 6*10, 9*23, &sprite6_9, std6x9_, 6, 0, gioBits);        
+    }
+    
+}
+
+static int irqCounts[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+static char hex4[] = "xxxx";
+
+void updateInt(int i) {
+    int x, y, op;  
+    
+    x = i & 7;
+    y = i >> 3;
+    
+    stringPutHexA(hex4, irqCounts[i], 4);
+    graphicsStringA(&screenBitmap, (10 + x*5)*6, (19+y)*9, &sprite6_9, std6x9_, 6, 0, hex4);    
+}
+
+void intsub() {
+    int i=0,c=0;
+    c = interruptsGetCausesA();
+    c = c | (~interruptsGetMaskA());
+    
+    for (i=0;i<32;i++) {
+        if (!(c&1)) break;
+        c = c>>1;
+    }
+
+    irqCounts[i]++;
+
+//    updateInt(i);
+
+    interruptsResetIRQA(i);
 }
 
