4a95e7c17b179a158d91f25077acf68b49251faa
oxygen77 <>
2004-08-08 10:27:51 +0000

    small bug fixes in makfile and config

--- gui/microwindows-0.90/src/include/av3xx_common.h
+++ gui/microwindows-0.90/src/include/av3xx_common.h
@@ -98,10 +98,12 @@ struct av_peak {
 #define AV_DSP_START_MP3       _IOW(AV_OP_IOC_MAGIC,34, int)
 #define AV_DSP_STOP_MP3        _IOW(AV_OP_IOC_MAGIC,35, int)
 #define AV_DSP_FRAME_CNT       _IOR(AV_OP_IOC_MAGIC,36, int)
-#define AV_DSP_IN_PEAK         _IOW(AV_OP_IOC_MAGIC,37, int)
-#define AV_DSP_OUT_PEAK        _IOW(AV_OP_IOC_MAGIC,38, int)
+#define AV_DSP_IN_PEAK         _IOW(AV_OP_IOC_MAGIC,37, int) /* normalized version range=0->100 */
+#define AV_DSP_OUT_PEAK        _IOW(AV_OP_IOC_MAGIC,38, int) /* normalized version range=0->100 */
+#define AV_DSP_IN_PEAK_REAL    _IOW(AV_OP_IOC_MAGIC,39, int) /* direct read: range=0->0x7FFF    */
+#define AV_DSP_OUT_PEAK_REAL   _IOW(AV_OP_IOC_MAGIC,40, int) /* direct read: range=0->0x7FFF    */
 
-#define AV_OP_IOC_MAXNR     38
+#define AV_OP_IOC_MAXNR     40
 
 /* MAJOR / MINOR */
 
