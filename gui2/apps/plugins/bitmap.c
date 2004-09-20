8143137c2f7784507d31e8fd226d4f0a36e1796c
schoki2 <>
2004-09-20 19:27:17 +0000

    example for using the drawBitmap function-

---
+++ gui2/apps/plugins/bitmap.c
@@ -0,0 +1,61 @@
+#include "graphics.h"
+#include "events.h"
+#include "avwm.h"
+
+#define BLACK 1
+#define WHITE 16
+
+#define true 1
+#define false 0
+
+struct client_operations * cops;
+
+int stop = 0;
+
+static unsigned char test[7][7] =
+    { {0xB9, 0xB9, 0xB9, 0xB9, 0xB9, 0xB9, 0xB9},
+      {0xB9, 0xff, 0xff, 0xff, 0xff, 0xff, 0xB9},
+      {0xB9, 0xff, 0xB9, 0xff, 0xB9, 0xff, 0xB9},
+      {0xB9, 0xff, 0xff, 0xB9, 0xff, 0xff, 0xB9},
+      {0xB9, 0xff, 0xB9, 0xff, 0xB9, 0xff, 0xB9},
+      {0xB9, 0xff, 0xff, 0xff, 0xff, 0xff, 0xB9},
+      {0xB9, 0xB9, 0xB9, 0xB9, 0xB9, 0xB9, 0xB9} };
+
+
+static BITMAP testB = {(unsigned int) test, 7, 7, 0, 0};
+
+
+int eventHandler(int evt)
+{
+    int i = 0;
+
+        switch (evt) {
+            case BTN_OFF:
+	         case EVT_QUIT:
+                /* get out of here */
+          		 stop=1;
+                break;
+        }
+}
+
+/* called function from outside */
+int main(int argc,char * * argv)
+{
+    int w, h, i;
+
+	 REGISTER(cops,eventHandler);
+    PACK(cops);
+
+	 cops->fillRect(WHITE,0, 0, LCD_WIDTH, LCD_HEIGHT);
+
+	 for(w = 0; w < LCD_WIDTH; w+=7)
+   	 for(h = 0; h < LCD_HEIGHT; h+=7)
+         cops->drawBITMAP (&testB, w, h);
+
+    while (!stop)
+ 	 {
+	 }
+
+    return ;
+}
+
