681873b673422ace77f7a8d02105b3e7ba15679c
oxygen77 <>
2004-09-27 10:14:45 +0000

    adding a general config header

---
+++ gui2/include/config.h
@@ -0,0 +1,22 @@
+/*
+* config.h
+*
+* linav - http://linav.sourceforge.net
+* Copyright (c) 2004 by Christophe THOMAS
+*
+* All files in this archive are subject to the GNU General Public License.
+* See the file COPYING in the source tree root for full license agreement.
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express of implied.
+*
+*/
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define AV_SCREEN   1
+#define X11_SCREE2  2
+
+#define GTYPE AV_SCREEN
+
+#endif
