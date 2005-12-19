3805aee7235f2036574fdd857ab7f001c6ce8180
oxygen77 <>
2005-12-19 21:06:08 +0000

    bad ci

---
+++ mediOS/include/int_timer.h
@@ -0,0 +1,26 @@
+/* 
+*   include/int_timer.h
+*
+*   MediOS project
+*   Copyright (c) 2005 by Christophe THOMAS (oxygen77 at free.fr)
+*
+* All files in this archive are subject to the GNU General Public License.
+* See the file COPYING in the source tree root for full license agreement.
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express of implied.
+*/
+
+#ifndef __INT_TIMER_H
+#define __INT_TIMER_H
+
+#define TIMER_MODE_BAT        0
+#define TIMER_MODE_DC         1
+
+#define LCD_TIMER             0
+#define HD_TIMER              1
+#define HALT_TIMER            2
+
+#define MODE_ENABLE           0
+#define MODE_DISABLE          1
+
+#endif
