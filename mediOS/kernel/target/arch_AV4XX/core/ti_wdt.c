8970bad2298937cd8a35e665cfad92d99c444042
oxygen77 <>
2006-09-12 20:02:53 +0000

    missing core folder

---
+++ mediOS/kernel/target/arch_AV4XX/core/ti_wdt.c
@@ -0,0 +1,26 @@
+/*
+*   kernel/target/arch_GMINI4XX/ti_wdt.c
+*
+*   MediOS project
+*   Copyright (c) 2005 by Christophe THOMAS (oxygen77 at free.fr)
+*
+* All files in this archive are subject to the GNU General Public License.
+* See the file COPYING in the source tree root for full license agreement.
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express of implied.
+*/
+
+#include <sys_def/stddef.h>
+#include <sys_def/types.h>
+
+#include <kernel/io.h>
+#include <kernel/hardware.h>
+#include <kernel/kernel.h>
+#include <kernel/ti_wdt.h>
+
+void arch_wdt_init(void)
+{
+}
+
+
+
